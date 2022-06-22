eb7c313aca93743e2a2d398c39413ea9c5b29ffe
Kawana T Small <jazzytech@Kawanas-MacBook-Pro.local>
2022-06-22 17:02:11 -0400

    commit tree c932028a3a407e50944fb1aea9e773b1e9c4b054

---
+++ index.js
@@ -0,0 +1,39 @@
+function receivesAFunction(callback) {
+console.log (callback());
+}
+function returnsANamedFunction(){
+  return function juice() {}
+}
+function returnsAnAnonymousFunction(){
+  return function() {}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function receivesAFunction(chai, spy) {
+  return chai(spy);
+}
+function returnsANamedFunction() {
+  return function index() {};
+}
+function returnsAnAnonymousFunction(){
+    
+   return function () {
+
+   }
+}*/
